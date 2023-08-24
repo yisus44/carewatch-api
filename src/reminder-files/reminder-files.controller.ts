b980065c74532e8311d42f150c00b9c111db1f66
yisus44 <jesusadrian1953.1@gmail.com>
2023-08-23 22:07:21 -0600

    reminders wip of revamp endpoints

--- src/reminder-files/reminder-files.controller.ts
+++ src/reminder-files/reminder-files.controller.ts
@@ -15,6 +15,9 @@ import { CreateReminderFileDto } from './dto/create-reminder-file.dto';
 import { UpdateReminderFileDto } from './dto/update-reminder-file.dto';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
 import { AuthGuard } from 'src/auth/guards/auth.guard';
+import { Permissions } from 'src/user-groups/decorators/permission.decorator';
+import { Permission } from 'src/user-groups/enums/permission.enum';
+import { PaginateReminderDto } from 'src/reminders/dto/paginate-reminder.dto';
 
 @UseGuards(AuthGuard)
 @Controller('reminder-files')
@@ -22,30 +25,40 @@ export class ReminderFilesController {
   constructor(private readonly reminderFilesService: ReminderFilesService) {}
 
   @Post()
-  create(@Body() createReminderFileDto: CreateReminderFileDto) {
+  @Permissions(Permission.uploadPermissionFile)
+  create(
+    @Query('groupId') groupId: number,
+    @Body() createReminderFileDto: CreateReminderFileDto,
+  ) {
     return this.reminderFilesService.create(createReminderFileDto);
   }
 
+  @Permissions(Permission.readPermissionFile)
   @Get()
-  findAll(@Query() paginationDto: PaginationDto) {
-    return this.reminderFilesService.findPaginated(paginationDto);
+  findAll(@Query() paginateReminderDto: PaginateReminderDto) {
+    return this.reminderFilesService.findPaginated(paginateReminderDto, {
+      reminderId: paginateReminderDto.reminderId,
+    });
   }
 
+  @Permissions(Permission.readPermissionFile)
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number) {
-    return this.reminderFilesService.findOneById(+id);
+    return this.reminderFilesService.findOneBy({ id });
   }
 
+  @Permissions(Permission.uploadPermissionFile)
   @Patch(':id')
   update(
     @Param('id', ParseIntPipe) id: number,
     @Body() updateReminderFileDto: UpdateReminderFileDto,
   ) {
-    return this.reminderFilesService.update(+id, updateReminderFileDto);
+    return this.reminderFilesService.updateBy({ id }, updateReminderFileDto);
   }
 
+  @Permissions(Permission.deletePermissionFile)
   @Delete(':id')
   remove(@Param('id', ParseIntPipe) id: number) {
-    return this.reminderFilesService.remove(+id);
+    return this.reminderFilesService.removeBy({ id });
   }
 }
---
+++ src/reminders/dto/paginate-reminder.dto.ts
@@ -0,0 +1,10 @@
+import { Type } from 'class-transformer';
+import { IsNotEmpty, IsPositive } from 'class-validator';
+import { PaginateGroupDto } from 'src/user-groups/dto/paginate-group.dto';
+
+export class PaginateReminderDto extends PaginateGroupDto {
+  @IsNotEmpty()
+  @IsPositive()
+  @Type(() => Number)
+  reminderId: number;
+}
